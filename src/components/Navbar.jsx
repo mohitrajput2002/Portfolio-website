aa338f37abe6b7378012de46ddf69c6a26360320
Mohit Singh Rajput <67924939+mohitrajput2002@users.noreply.github.com>
2023-08-29 01:05:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Mohit &nbsp;
-            <span className='sm:block hidden'>Portfolio</span>
+            <span className='sm:block hidden'><strong>MR </strong>Portfolio</span>
           </p>
         </Link>
 
