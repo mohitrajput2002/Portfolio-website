2109e303d371ef6524a56bbd5e28165a5cfcba56
Mohit Singh Rajput <67924939+mohitrajput2002@users.noreply.github.com>
2023-11-07 14:02:09 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,75 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { useState, useEffect } from "react";
 
 const Hero = () => {
+  const [typedText, setTypedText] = useState("");
+  const typedItems = ["Developer", "Freelancer", "Designer", "Learner"];
+  const [itemIndex, setItemIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
+
+  useEffect(() => {
+    const typeItem = () => {
+      if (charIndex < typedItems[itemIndex].length) {
+        setTypedText((prevText) => prevText + typedItems[itemIndex][charIndex]);
+        setCharIndex(charIndex + 1);
+      } else {
+        setIsTyping(false);
+        setTimeout(() => {
+          setIsTyping(true);
+          setItemIndex((itemIndex + 1) % typedItems.length);
+          setCharIndex(0);
+          setTypedText("");
+        }, 1000); // Delay before typing the next item
+      }
+    };
+
+    const typingInterval = setInterval(typeItem, 100); // Typing speed
+
+    return () => clearInterval(typingInterval);
+  }, [charIndex, itemIndex]);
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Mohit Singh Rajput</span>
+            Hi, I'm <span className="text-[#915EFF]">Mohit Singh Rajput</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm an aspiring software & web developer who loves using web technologies to
-            create cool applications. I'm always eager to learn and improve my
-            skills. "Bring on the challenges, I'm ready to soak up knowledge!"
+            I'm{" "}
+            <span
+              className="typed"
+              aria-hidden="true"
+              style={{
+                backgroundImage: "linear-gradient(to bottom, rgba(245, 202, 153, 0.5), rgba(245, 202, 153, 0.5))",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 8px",
+                backgroundPosition: "0 100%",
+                color: "red",
+                display: "inline-block",
+                fontWeight: "bold"
+              }}
+            >
+              {typedText}
+            </span>
+            <span 
+              className="typed-cursor" 
+              aria-hidden="true"
+            >
+              |
+            </span>
+            <br />
+            <b>Bring on the challenges, I'm ready to soak up knowledge!</b>
           </p>
         </div>
       </div>
@@ -29,9 +77,9 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -41,7 +89,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
